e38474a4e0c2f8d1277ca45b0b611e0dbbf9a033
J_theGit <jevgenij.turonok@gmail.com>
2019-05-25 12:59:52 +0100

    changed routes to use mongo

--- routes/genres.js
+++ routes/genres.js
@@ -1,13 +1,49 @@
 const express = require('express');
-const router = express.Router();
+const mongoose = require('mongoose');
 const Joi = require('joi');
+const debug = require('debug')('app:genres');
+const config = require('config');
 
-const genres = [
-    { id: 1, name: 'comedy' },
-    { id: 2, name: 'action' },
-    { id: 3, name: 'drama' },
-    { id: 4, name: 'horror' },
-];
+const router = express.Router();
+const mongourl = config.get('mongo-endpoint') + '/vidly';
+
+mongoose.connect(mongourl, { useNewUrlParser: true })
+    .then(() => debug('connected to mongo'));
+
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    }
+});
+
+const Genres = mongoose.model('Genre', genreSchema);
+
+async function getGenres() {
+    return await Genres.find();
+}
+
+async function getGenre(id) {
+    return await Genres.findById(id);
+}
+
+async function insertGenre(name) {
+    const genre = new Genres();
+    genre.name = name;
+    return await genre.save();
+}
+
+async function updateGenre(id, name) {
+    const genre = await Genres.findById(id);
+    genre.name = name;
+    return await genre.save();
+    //return await Genres.findById(id);
+}
+
+async function deleteGenre(id) {
+    return await Genres.findByIdAndRemove(id);
+}
 
 function validateGenre(input) {
     const schema = {
@@ -16,45 +52,64 @@ function validateGenre(input) {
     return Joi.validate(input, schema);
 }
 
-router.get('/', (req, res) => {
-    res.send(genres);
-});
-
-router.get('/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('genre couldn\'t be found');
-    res.send(genre);
-});
-
-router.post('/', (req, res) => {
-    const { error } = validateGenre(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
+router.get('/', async function(req, res) {
+    try {
+        const results = await getGenres();
+        res.send(results);
     }
-    genres.push(genre);
-    res.send(genre);
+    catch (e) {
+        debug(e);
+    }
 });
 
-router.put('/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('genre couldn\'t be found');
+router.get('/:id', async (req, res) => {
+    try {
+        const genre = await getGenre(req.params.id);
+        if (!genre) return res.status(404).send('genre couldn\'t be found');
+        res.send(genre);
+    }
+    catch (e) {
+        res.status(404).send('genre couldn\'t be found');
+    }
+});
 
+router.post('/', async (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     
-    genres.name = req.body.name;
-    res.send(genre);
+    try {
+        const genre = await insertGenre(req.body.name);
+        res.send(genre);
+    }
+    catch (e) {
+        debug(e);
+        return res.status(500).send('unable to insert genre');
+    }
 });
 
-router.delete('/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('genre couldn\'t be found');
+router.put('/:id', async (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-    const i = genres.indexOf(genre);
-    res.send(genres.splice(i, 1));
+    try {
+        const genre = await updateGenre(req.params.id, req.body.name);
+        res.send(genre);
+    }
+    catch (e) {
+        res.status(404).send('genre couldn\'t be found');
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const genre = await deleteGenre(req.params.id);
+        if (!genre) return res.status(404).send('genre couldn\'t be found');
+        res.send(genre);
+    }
+    catch (e) {
+        res.status(404).send('genre couldn\'t be found');
+
+    }
 });
 
 module.exports = router;
