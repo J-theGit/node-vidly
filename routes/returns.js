cf58421e1bf6f644d0b5a6c93dcd22cca0bcf282
J_theGit <jevgenij.turonok@gmail.com>
2019-06-03 19:34:01 +0100

    passed test for id validation

--- routes/returns.js
+++ routes/returns.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const Joi = require('joi');
 const debug = require('debug')('app:routes:returns');
 const auth = require('../middleware/auth');
 const rentals = require('../models/rentals');
 const router = express.Router();
 
+function validateId(obj) {
+    const schema = {
+        id: Joi.objectId().required()
+    }
+    return Joi.validate(obj, schema);
+}
+
 router.post('/:id', auth, async (req, res) => {
+    const { error } = validateId(req.params);
+    if (error) return res.status(400).send(error.details[0].message);
+
     const rental = await rentals.submit(req.params.id);
     if (!rental) return res.status(400).send('rental was already returned');
 
